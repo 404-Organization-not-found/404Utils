a05ff585a663f841ee21a16a8f7229c3f587b9b4
BlackDev <tjorgefaerber@outlook.com>
2025-01-24 21:40:28 +0000

    Refactor UserInfoCommand and ServerInfoCommand to use EmbedBuilder for better message formatting; add .env and IDE configuration files

--- src/main/java/dev/blackdev/commands/ServerInfoCommand.java
+++ src/main/java/dev/blackdev/commands/ServerInfoCommand.java
@@ -1,16 +1,14 @@
-/*
-//Copyright 404
-//Licensed under MIT License
-*/
-
-
 package dev.blackdev.commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.Color;
+import java.util.stream.Collectors;
 
 public class ServerInfoCommand extends ListenerAdapter {
 
@@ -22,10 +20,23 @@ public class ServerInfoCommand extends ListenerAdapter {
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (event.getName().equals("serverinfo")) {
             var guild = event.getGuild();
-            event.reply("Server Info:\n" +
-                    "Name: " + guild.getName() + "\n" +
-                    "ID: " + guild.getId() + "\n" +
-                    "Members: " + guild.getMemberCount()).queue();
+            var onlineMembers = guild.getMembers().stream().filter(member -> member.getOnlineStatus().getKey().equals("online")).count();
+            var offlineMembers = guild.getMembers().stream().filter(member -> member.getOnlineStatus().getKey().equals("offline")).count();
+            var totalChannels = guild.getChannels().size();
+            var totalRoles = guild.getRoles().size();
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(Color.BLACK);
+            embed.setTitle("Server Info");
+            embed.setThumbnail(guild.getIconUrl());
+            embed.addField("Name", guild.getName(), false);
+            embed.addField("ID", guild.getId(), false);
+            embed.addField("Members", String.valueOf(guild.getMemberCount()), false);
+            embed.addField("Online Members", String.valueOf(onlineMembers), false);
+            embed.addField("Offline Members", String.valueOf(offlineMembers), false);
+            embed.addField("Channels", String.valueOf(totalChannels), false);
+            embed.addField("Roles", String.valueOf(totalRoles), false);
+            event.replyEmbeds(embed.build()).queue();
         }
     }
 }
--- src/main/java/dev/blackdev/commands/UserInfoCommand.java
+++ src/main/java/dev/blackdev/commands/UserInfoCommand.java
@@ -1,17 +1,14 @@
-/*
-//Copyright 404
-//Licensed under MIT License
-*/
-
-
 package dev.blackdev.commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.Color;
 
 public class UserInfoCommand extends ListenerAdapter {
 
@@ -25,10 +22,13 @@ public class UserInfoCommand extends ListenerAdapter {
         if (event.getName().equals("userinfo")) {
             var user = event.getOption("user").getAsUser();
             var member = event.getGuild().getMember(user);
-            event.reply("User Info:\n" +
-                    "Name: " + user.getName() + "\n" +
-                    "ID: " + user.getId() + "\n" +
-                    "Joined: " + (member != null ? member.getTimeJoined() : "N/A")).queue();
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(Color.BLACK);
+            embed.setTitle("User Info");
+            embed.addField("Name", user.getName(), false);
+            embed.addField("ID", user.getId(), false);
+            embed.addField("Joined", member != null ? member.getTimeJoined().toString() : "N/A", false);
+            event.replyEmbeds(embed.build()).queue();
         }
     }
 }
